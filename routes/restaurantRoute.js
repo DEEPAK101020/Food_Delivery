511ce0e10c3eb178470530993598c47d1b604067
DEEPAK101020 <deepakkumarktr1998@gmail.com>
2024-03-20 14:26:00 +0530

    restaurants routes done

--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -51,16 +51,56 @@ RestaurantRoute.get("/restaurants/:id/menu",async(req,res)=>{
     }
 })
 
-RestaurantRoute.post("/restaurants:id/menu",async(req,res)=>{
-const userId=req.params.id;
+//post in specific menu
+RestaurantRoute.post("/:id/menu", async (req, res) => {
+
+    const restaurantId = req.params.id;
+    const { name, description, price, image } = req.body;
+
     try {
-        const menu=await RestaurantModel.findById(req.params.id);
-        
-        res.status(200).json(menu.menu)
-    } catch (error) {
-        res.status(500).json({err:error})
+        const restaurant = await RestaurantModel.findById(restaurantId);
+
+        if (!restaurant) {
+            return res.status(404).json({ msg: "Restaurant not found" });
+        }
+
+        restaurant.menu.push({ name, description, price, image });
+        await restaurant.save();
+
+        res.status(201).json({ msg: "new item added to menu", menu: restaurant.menu });
+    } catch (err) {
+        res.status(500).json({ msg: "Failed to add", error: err });
     }
-})
+});
+
+
+//delete a menu
+
+// delete a menu item
+RestaurantRoute.delete("/:restaurantId/menu/:itemId", async (req, res) => {
+    const restaurantId = req.params.restaurantId;
+    const itemId = req.params.itemId;
+
+    try {
+
+        const restaurant = await RestaurantModel.findById(restaurantId);
+        if (!restaurant) {
+            return res.status(404).json({ msg: "Restaurant not found" });
+        }
+        const index = restaurant.menu.findIndex(item => item._id.toString() === itemId);
+        if (index === -1) {
+            return res.status(404).json({ msg: "Menu item not found" });
+        }
+        restaurant.menu.splice(index, 1);
+
+        await restaurant.save();
+
+        res.status(202).json({ msg: "Menu item deleted", menu: restaurant.menu });
+    } catch (err) {
+        res.status(500).json({ msg: "Failed to delete menu item", error: err });
+    }
+});
+
 
 
 module.exports={
