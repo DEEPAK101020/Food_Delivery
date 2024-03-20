7573beed123cae4c935ce9d615be398fe3389035
DEEPAK101020 <deepakkumarktr1998@gmail.com>
2024-03-20 13:17:22 +0530

    user routes done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -27,6 +27,57 @@ Userrouter.post("/register",async(req,res)=>{
     }
 })
 
+Userrouter.post("/login",async(req,res)=>{
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            bcrypt.compare(password, user.password, (err, pass) => {
+                if (err) {
+                    return res.status(401).json({ msg: "Invalid credentials" });
+                }
+                if (pass) {
+                    const secret_key = process.env.secret_key;
+                    const token = jwt.sign({ userID: user._id }, secret_key, { expiresIn: "7d" });
+                    res.status(201).json({ token });
+                } else {
+                    res.status(401).json({ msg: "Invalid credentials" });
+                }
+            });
+        } else {
+            res.status(401).json({ msg: "User does not exist" });
+        }
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+})
+
+Userrouter.put("/:id/reset",async(req,res)=>{
+        const userId = req.params.id;
+        const { password, newPassword } = req.body;
+    
+        try {
+            const user = await UserModel.findById(userId);
+            if (!user) {
+                return res.status(404).json({ msg: "User not found" });
+            }
+            bcrypt.compare(password, user.password, async (err, pass) => {
+                if (err ||!pass) {
+                    return res.status(401).json({ msg: "Invalid current password" });
+                }
+                const hashedNewPassword = await bcrypt.hash(newPassword, 5);
+                user.password = hashedNewPassword;
+                await user.save();
+    
+                res.status(204).send("password updated sucessfully"); 
+            });
+        } catch (error) {
+            res.status(500).json({ err: error.message });
+        }
+    
+})
+
+
 module.exports={
     Userrouter
 }
